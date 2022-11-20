e3397d6c1a5c37ac01a2d74bf8d0e7ce7f2cab9c
berserk <gaysin.iln@gmail.com>
2022-11-20 16:51:05 +0400

    Create my1.py

---
+++ my1.py
@@ -0,0 +1,18 @@
+num=int(input("Enter the number: "))
+
+def PerfectNum():
+    sum = 0
+    for i in range(1, num//2+1):
+
+        if num % i == 0:
+            sum += i
+        print(sum)
+        if sum == num:
+            return True
+            return False
+
+
+if PerfectNum(num):
+    print("Введенное число: ", num, " Совершенное")
+else:
+    print("Введенное число: ", num, " не совершенное")
